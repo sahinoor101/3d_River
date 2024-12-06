bdc5abac351dad7a50c5126c4cd425e44481312b
sahinoor101 <sahinoor101@gmail.com>
2024-12-06 11:48:32 +0530

    path of longest river(Without generating random map

--- Map.java
+++ Map.java
@@ -1,109 +1,121 @@
 class Map
 {
-    static int river[][]={{0,0,0,0,0,0},{0,0,0,0,0,0},{0,0,0,0,0,0},{0,0,0,0,0,0},{0,0,0,0,0,0},{0,0,0,0,0,0}};
     public static void main(String args[])
     {
         int map[][]={{3,2,6,3,8,4},{2,3,9,9,5,8},{4,5,7,7,1,5},{7,7,8,9,3,3},{5,7,8,3,4,2},{6,7,3,8,9,6}};
         int i,j;
         Map ob=new Map();
         //map=ob.getMap();
-        int river[][];
-        river=ob.river(1, 2, map);
         for(i=0;i<map.length;i++)
         {
             for(j=0;j<map[0].length;j++)
             {
-                System.out.print(river[i][j]+" ");
+                System.out.print(map[i][j]+" ");
             }
             System.out.println();
         }
+        System.out.println("Source block : ("+1+","+2+")");
+        ob.river(1, 2, map);
     }
-    public int[][] river(int x,int y,int map[][])
+    public void river(int x,int y,int map[][])
     {
         //int river[][]={{0,0,0,0,0,0},{0,0,0,0,0,0},{0,0,0,0,0,0},{0,0,0,0,0,0},{0,0,0,0,0,0},{0,0,0,0,0,0}};
         Map ob=new Map();
-        char max=ob.max(x+1,x-1,y+1,y-1,map);
-        if(max == 'x' )
+        if(ob.isValid( x, y, map)== true )
         {
-            return river;
-        }
-        if(max == 't' && ob.isValid( x, y, map)== true)
-        {
+            char ch=ob.max(x+1,x-1,y+1,y-1,map,map[x][y]);
             map[x][y]=0;
-            river[x][y]=1;
-            ob.river(x+1, y, map);
+            switch (ch) {
+                case 'r':
+                    ob.river(x, y+1, map);
+                    break;
+                case 'l':
+                    ob.river(x, y-1, map);
+                    break;
+                case 'b':
+                    ob.river(x+1, y, map);
+                    break;
+                case 't':
+                    ob.river(x-1, y, map);
+                    break;
+                default:
+                    break;
+            }
         }
-        if(max == 'b'  && ob.isValid( x, y, map)== true)
-        {
-            map[x][y]=0;
-            river[x][y]=1;
-            ob.river(x-1, y, map);
-        }
-        if(max == 'r' && ob.isValid( x, y, map)== true)
-        {
-            map[x][y]=0;
-            river[x][y]=1;
-            ob.river(x, y+1, map);
-        }
-        if(max== 'l' && ob.isValid( x, y, map)== true)
-        {
-            map[x][y]=0;
-            river[x][y]=1;
-            ob.river(x, y-1, map);
-        }
-        return river;
     }
-    public char max(int x1,int x2,int y1,int y2,int map[][])
+    public char max(int x1,int x2,int y1,int y2,int map[][],int n)
     {
         Map ob=new Map();
         int a,b,c,d;
-        if(ob.isValid(x1, y1-1, map)== false)
+        if(ob.isValid(x1, y1-1, map)== false )
         {
             a=0;
         }
-        else
+        else if(map[x1][y1-1]<=n)
         {
             a=map[x1][y1-1];
+        }
+        else
+        {
+            a=0;
         }
         if(ob.isValid(x2, y1-1, map)== false)
         {
             b=0;
         }
-        else
+        else if(map[x2][y1-1]<=n)
         {
             b=map[x2][y1-1];
+        }
+        else
+        {
+            b=0;
         }
         if(ob.isValid(x1-1, y1, map)== false)
         {
             c=0;
         }
-        else
+        else if(map[x1-1][y1]<=n)
         {
             c=map[x1-1][y1];
+        }else
+        {
+            c=0;
         }
         if(ob.isValid(x1-1, y2, map)== false)
         {
             d=0;
         }
-        else
+        else if(map[x1-1][y2]<=n)
         {
             d=map[x1-1][y2];
+        }else
+        {
+            d=0;
+        }
+        if(a==0 && b==0 && c==0 && d==0)
+        {
+            return 'x';
         }
         int max= Math.max(Math.max(a,b),Math.max(c,d));
         if(a == max)
         {
-            return 't';
-        }
-        if(b == max)
-        {
+            System.out.println("bottom("+x1+","+(y1-1)+")");
             return 'b';
         }
-        if(c == max)
+        else if(b == max)
         {
+            System.out.println("top("+x2+","+(y1-1)+")");
+            return 't';
+        }
+        else if(c == max)
+        {
+            System.out.println("Right("+(x1-1)+","+y1+")");
             return 'r';
         }
-        if(d == max)
+        else if(d == max)
         {
+            System.out.println("Left("+(x1-1)+","+y2+")");
             return 'l';
         }
         return 'x';
